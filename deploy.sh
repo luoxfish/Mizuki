e003aac0cfe60a71c4eca4aae529f49343f41c7f
luoxfish <2108268486@qq.com>
2026-03-09 01:23:50 +0800

    Update 2026-03-09 01:23:49

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 获取提交信息，若未传入参数则使用默认信息
-COMMIT_MSG=${1:-"更新博客 $(date '+%Y-%m-%d %H:%M:%S')"}
+COMMIT_MSG=${1:-"Update $(date '+%Y-%m-%d %H:%M:%S')"}
 
 git add .
 git commit -m "$COMMIT_MSG"
