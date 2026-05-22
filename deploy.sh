77fecae5b9333669c8daf40cf6e19d76a8375d38
luoxfish <2108268486@qq.com>
2026-05-22 20:16:30 +0800

    添加博客编辑器

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# 获取提交信息，若未传入参数则使用默认信息
-COMMIT_MSG=${1:-"Update $(date '+%Y-%m-%d %H:%M:%S')"}
+COMMIT_MSG=${1:-"更新博客 $(date '+%Y-%m-%d %H:%M:%S')"}
+
+git pull origin master --rebase
 
 git add .
 git commit -m "$COMMIT_MSG"
